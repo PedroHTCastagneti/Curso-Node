8a44b4d90d77604ecd05a278c67b191482154489
Pedro <pedrotoneti150@gmail.com>
2024-02-16 16:33:05 -0300

    implementandoBody

--- curso-node/node/index.js
+++ curso-node/node/index.js
@@ -1,1 +1,39 @@
-console.log("Hello world");
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get('/projetos', (req,res) => {
+    return res.json({
+        name:"Projeto 1",
+        owner:"Pedro"
+    });
+});
+
+app.post('/projetos', (req,res) => {
+
+    console.log(req.body)
+    return res.json({
+        mensagem:"ola",
+        name: req.body.name ,
+        owner: req.body.owner
+    });
+
+});
+
+app.put('/projetos/:id', (req,res) => {
+    console.log(req.body);
+    return res.json({
+        mensagem:"isnira uma rota",
+    });
+});
+
+app.delete('/projetos/:id', (req,res) => {
+    console.log(req.params);
+    return res.json({
+        mensagem:"delete",
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Servidor esta funcionando");
+});
