7a80137fa2b85636d6450a2089ad80f7cae86319
Pedro <pedrotoneti150@gmail.com>
2024-02-21 19:35:20 -0300

    prismaAvancado

--- curso-node/node/index.js
+++ curso-node/node/index.js
@@ -2,24 +2,35 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const server=  [];
+const { Prisma, PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
-app.get('/projetos', (req,res) => {
-    return res.json(
-        server
-    );
+ 
+
+app.get('/projetos', async (req,res) => {
+
+    const posts = await prisma.post.findMany({include:{author:true}});
+
+    return res.json(posts)
 });
 
-app.post('/projetos', (req,res) => {
-    server.push(req.body);
-
-    console.log(req.body)
-    return res.json({
-        mensagem:"ola",
-        name: req.body.name ,
-        owner: req.body.owner
-    });
-
+app.post('/usuario', async(req,res) =>{
+    const result = await prisma.user.create({data: req.body});
+    return res.json(result);
 });
+
+app.post('/post', async (req, res) => {
+    const { title, content,authorEmail} = req.body;
+    const result = await prisma.post.create({
+      data: {
+        title,
+        content,
+        published: false,
+        author: { connect: { email: authorEmail } },
+      },
+    })
+    res.json(result)
+  })
 
 app.put('/projetos/:id', (req,res) => {
     for (let index = 0; index < server.length; index++) {
